d8feb90a333e353e860fae2b159646c2503898a9
Eduardo Almeida da Silva <eduardoalmeida@EDUARDOS-MBP.station>
2022-05-04 00:35:22 +0100

    first commit

---
+++ src/main/java/com/almeidait/bancklineapi/BancklineApiApplication.java
@@ -0,0 +1,13 @@
+package com.almeidait.bancklineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancklineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almeidait/bancklineapi/BancklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almeidait.bancklineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
